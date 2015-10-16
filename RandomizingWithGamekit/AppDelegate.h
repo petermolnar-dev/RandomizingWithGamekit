ed312a52900663627abef0ef1ecc6d7eca88d5f6
Peter Molnar <peter@petermolnar.hu>
2015-10-16 20:54:39 +0200

    Initial Commit

---
+++ RandomizingWithGamekit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RandomizingWithGamekit
+//
+//  Created by Peter Molnar on 16/10/2015.
+//  Copyright © 2015 Peter Molnar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RandomizingWithGamekit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RandomizingWithGamekit
+//
+//  Created by Peter Molnar on 16/10/2015.
+//  Copyright © 2015 Peter Molnar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
