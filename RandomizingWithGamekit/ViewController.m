a6beca3e7b3dbb217ff7ede26d72dec19d5655ce
Peter Molnar <peter@petermolnar.hu>
2015-10-16 21:56:44 +0200

    First draft of viewcontroller

--- RandomizingWithGamekit/ViewController.m
+++ RandomizingWithGamekit/ViewController.m
@@ -9,14 +9,42 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *times1;
+@property (strong, nonatomic) IBOutlet UILabel *times2;
+@property (strong, nonatomic) IBOutlet UILabel *times3;
+@property (strong, nonatomic) IBOutlet UILabel *times4;
+@property (strong, nonatomic) IBOutlet UILabel *times5;
+@property (strong, nonatomic) IBOutlet UILabel *times6;
+@property (strong, nonatomic) IBOutlet UILabel *times7;
+@property (strong, nonatomic) IBOutlet UILabel *times8;
+@property (strong, nonatomic) IBOutlet UILabel *times9;
+@property (strong, nonatomic) IBOutlet UILabel *times0;
+
 
 @end
 
 @implementation ViewController
 
+- (IBAction)startButtonPressed:(id)sender {
+    
+}
+
+- (IBAction)stopButtonPressed {
+}
+
+-(void)stratGeneratingRandomNumbers {
+//    set up a timer
+    // Ask for the nextInt
+
+// put it into the array
+// update the display
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+// set up an array of the UILables
+//    Set up a random generator with the given type and seed
 }
 
 - (void)didReceiveMemoryWarning {
